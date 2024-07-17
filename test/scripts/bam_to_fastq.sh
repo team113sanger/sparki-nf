58614d9792cdbe3438800eb66d3e6993abd66cd9
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2024-07-17 13:34:20 +0100

    Wrote code to convert BAM to FASTQ.

---
+++ test/scripts/bam_to_fastq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# BSUB -q normal
+# BSUB -o /lustre/scratch126/casm/team113da/users/jb62/projects/sparki-nf/test/logs/bam_to_fastq.out
+# BSUB -e /lustre/scratch126/casm/team113da/users/jb62/projects/sparki-nf/test/logs/bam_to_fastq.err
+# BSUB -J bam_to_fastq
+# BSUB -R "select[mem>20GB] rusage[mem=20GB] span[hosts=1]"
+# BSUB -M 20GB
+
+BAMS=$(ls -lh /lustre/scratch126/casm/team113da/projects/5765_2680_sebaceous_tumour_RNAseq_remap/bams/*.bam | head -5)
+OUTDIR="/lustre/scratch126/casm/team113da/users/jb62/projects/sparki-nf/test/data"
+
+module load samtools/1.14
+
+for BAM in BAMS; do
+    SAMPLE_NAME=$(basename ${BAM} ".bam")
+    echo "Running ${SAMPLE_NAME}"
+    samtools collate -u -O ${BAM} | samtools fastq -c 6 -@ 8 -1 ${OUTDIR}/${SAMPLE_NAME}_1.fq.gz -2 ${OUTDIR}/${SAMPLE_NAME}_2.fq.gz -0 /dev/null -s /dev/null -n
+done
